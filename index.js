ebca6b537667776e5120277b4b26a0fcc8ba3962
RENAN.BASTOS <renanbastos.tec@gmail.com>
2017-08-13 01:00:37 -0300

    [+] method run and pause action

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function like(){
-	document.querySelectorAll('button').forEach( item => {
-    if(item.getAttribute('aria-label') == 'Like'){
-      item.click()
-    }
-  });
+	document.querySelectorAll('button').forEach(
+        item => {
+            if(item.getAttribute('aria-label') == 'Like'){
+                item.click()
+            }
+        }
+    );
 };
+
+function run(time){
+    return setInterval(like, time);
+}
+
+let idInterval = run(1000); // 1 second
+
+function pause(id){
+    clearInterval(id);
+}
