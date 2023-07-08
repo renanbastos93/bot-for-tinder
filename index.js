76d957d3439badbd13ec95dbd35b8714c39574ee
Renan Bastos <renanbastos.tec@gmail.com>
2023-07-08 14:45:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function like(){
-    document.querySelectorAll('button').forEach(
-        item => {
-            if(item.getAttribute('aria-label') == 'Like'){
-                item.click()
-            }
-        }
-    );
+    document.querySelector("#u490315748 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-like-default\\) > button").click()
 };
 
 function run(time){
